caaf67fd201a70321660bdad0eb54c28323c91f7
Hans <vhansangelo@gmail.com>
2025-10-29 23:19:37 +0800

    ApplicationUser and VolunteerLog entities

---
+++ VolunteerHoursTracker.Infrastructure/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VolunteerHoursTracker.Infrastructure.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateRegistered { get; set; } = DateTime.UtcNow;
+
+        // Navigation property
+        public virtual ICollection<VolunteerLog> VolunteerLogs { get; set; } = new List<VolunteerLog>();
+    }
+}
---
+++ VolunteerHoursTracker.Infrastructure/Entities/VolunteerLog.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VolunteerHoursTracker.Infrastructure.Entities
+{
+    public class VolunteerLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
+        public string Activity { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0.5, 24)]
+        public double HoursWorked { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public string? Description { get; set; }
+
+        // Navigation property
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
